bda637b4fe170047e5c7a61e5fad7588fba65937
jukyellow <45334819+jukyellow@users.noreply.github.com>
2019-06-05 00:52:02 +0900

    java added

---
+++ back-end(java,spring)/01_Pattern_Matcher_ReplaceAll.java
@@ -0,0 +1,10 @@
+//Patternm,Matcher since JDK 1.4
+
+String originStr= "TEST/ ABC-STR 111 OK!";
+Pattern p = Pattern.compile("[^a-zA-Z0-9]*");
+Matcher m = p.matcher(errMsg);
+
+String newStr = m.replaceAll(""); //공백으로 치환
+
+System.out.println("originStr:"+originStr);
+System.out.println("newStr   :"+newStr);
