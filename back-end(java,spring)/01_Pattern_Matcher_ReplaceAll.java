64e30fbf8ee603c793201d176c291f90656508e3
UnKuk Joung <jukyellow@gmail.com>
2019-06-05 01:06:24 +0900

    Update 01_Pattern_Matcher_ReplaceAll.java

--- back-end(java,spring)/01_Pattern_Matcher_ReplaceAll.java
+++ back-end(java,spring)/01_Pattern_Matcher_ReplaceAll.java
@@ -1,10 +1,19 @@
-//Patternm,Matcher since JDK 1.4
+package com.test.juk;
 
-String originStr= "TEST/ ABC-STR 111 OK!";
-Pattern p = Pattern.compile("[^a-zA-Z0-9]*"); //숫자, 알파벳이 아닌 패턴만 
-Matcher m = p.matcher(errMsg);
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-String newStr = m.replaceAll(""); //공백으로 치환
+public class A1_Pattern_Matcher_ReplaceAll {
+	public static void main( String[] args ) {
+		//Patternm,Matcher since JDK 1.4
 
-System.out.println("originStr:"+originStr);
-System.out.println("newStr   :"+newStr);
+		String originStr= "TEST/ ABC-STR 111 OK!";
+		Pattern p = Pattern.compile("[^a-zA-Z0-9]*"); //숫자, 알파벳이 아닌 패턴만 
+		Matcher m = p.matcher(originStr);
+
+		String newStr = m.replaceAll(""); //공백으로 치환
+
+		System.out.println("originStr:"+originStr);
+		System.out.println("newStr   :"+newStr);
+	}
+}
