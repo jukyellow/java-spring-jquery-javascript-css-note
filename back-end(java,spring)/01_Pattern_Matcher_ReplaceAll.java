367b9b81b82a0cd9567a4dc5a3f82b1699f42815
UnKuk Joung <jukyellow@gmail.com>
2019-06-05 00:53:35 +0900

    Update 01_Pattern_Matcher_ReplaceAll.java

--- back-end(java,spring)/01_Pattern_Matcher_ReplaceAll.java
+++ back-end(java,spring)/01_Pattern_Matcher_ReplaceAll.java
@@ -1,7 +1,7 @@
 //Patternm,Matcher since JDK 1.4
 
 String originStr= "TEST/ ABC-STR 111 OK!";
-Pattern p = Pattern.compile("[^a-zA-Z0-9]*");
+Pattern p = Pattern.compile("[^a-zA-Z0-9]*"); //숫자, 알파벳이 아닌 패턴만 
 Matcher m = p.matcher(errMsg);
 
 String newStr = m.replaceAll(""); //공백으로 치환
